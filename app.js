6c900f3b447d6fb1bcc73e886b4f90fb4762d2e8
DanielFdez <danielfg1989@yahoo.es>
2015-03-18 12:25:26 +0000

    Primer commit
    Aplicación funcional, requiere algunas mejoras.

---
+++ app.js
@@ -0,0 +1,28 @@
+(function() {
+  var app = angular.module('gemStore', ['store-directives']);
+
+  app.controller('StoreController', ['$http', function($http){
+    var store = this;
+    store.products = [];
+    $http.get('store-products.json').success(function(data){
+        store.products = data;
+    });
+  }]);
+
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+		this.review.createdOn = Date.now();
+		product.reviews.push(this.review);
+
+		this.review = {};
+    };
+  });
+  
+  app.controller('BuyController', function() {
+	this.addBuy = function(product) {
+		alert('Gracias por su compra, pronto recibirá su pedido en la dirección indicada');
+	};
+  });
+})();
