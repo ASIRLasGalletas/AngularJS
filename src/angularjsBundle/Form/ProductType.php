776dd87aca6e0726e8092b9a22c5d7e3dae36bd2
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2015-03-22 19:14:42 +0000

    CRUD for Product entity

---
+++ src/angularjsBundle/Form/ProductType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace angularjsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProductType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('shine')
+            ->add('price')
+            ->add('rarity')
+            ->add('hardness')
+            ->add('faces')
+            ->add('images')
+            ->add('reviews')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'angularjsBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'angularjsbundle_product';
+    }
+}
